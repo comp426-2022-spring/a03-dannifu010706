36b34ef65820fe2f0f80a04558b40a20c2c0649d
Nifu Dan <dannifu010706@outlook.com>
2022-04-25 16:47:25 -0400

    a3

--- server.js
+++ server.js
@@ -1,84 +1,55 @@
-
+import { coinFlips, countFlips, coinFlip, flipACoin } from "./modules/coin.mjs"
+import minimist from 'minimist'
+import express from 'express'
 const app = express()
-var port = 5000
 
-app.get('/app/echo/:number',(req,res)=>{
-    res.status(200)/json({'message':req.params.number})
+var min = minimist(process.argv.slice(2))
+var port = 'port'
+const HTTP_PORT = min[port] || 5000
+
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
 })
-const server = app.listen(port,() => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+
+app.get('/app/', (req, res) => {
+    //Respond with status 200
+    res.statusCode = 200
+    //Respond with status message "OK"
+    res.statusMessage = 'OK'
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain'})
+    res.end(res.statusCode+ ' ' +res.statusMessage)
 })
-function coinFlip() {
-    return Math.random() > .5 ? ("heads") : ("tails")
-  }
 
-function coinFlips(flips) {
-    var arr = [];
-    for(let i =0 ; i<flips;i++){
-      arr.push(coinFlip());
-    }
-   return arr;
-  }
-
-  function countFlips(array) {
-    let result={};
-    array.forEach((item) => { 
-      if(result[item]){
-        result[item]++;
-      }else{
-        result[item]=1;
-      }
-    });
-    return result;
-   }
-
-   function flipACoin(call) {
-  
-    const results = {call: '', flip: '', result: ''}
-    results.call=call;
-    results.flip=coinFlip();
-    if(results.call==results.flip){
-      results.result="win";
-    }
-    else{
-      results.result="lose";
-    }
-    return results;
-    }
-  
-
-
-app.get('/app',(req,res)=> {
-   res.statusCode=200;
-   res.statusMessage='OK';
-   res.writeHead(res.statusCode,{'Content-Type':'text/plain'});
-   res.end(res.statusCode+' '+res.statusMessage)
-
+app.get('/app/flip/', (req, res) => {
+    res.statusCode = 200
+    var flip = coinFlip()
+    res.json({flip : flip})
+    res.writeHead( res.statusCode, { 'Content-Type' : 'application/json'})
 })
-app.get('/app/flip',(req,res)=>{
-    var flips = coinFlip()
+
+app.get('/app/flips/:number', (req, res) => {
+    var coins = coinFlips(req.params.number)
     
-    res.status(200).json({ 'flip':flip})
-})
-app.get('/app/flips/:number',(req,res)=>{
-    const flips = (req.params.number)
-    const coins=coinFlips(flips)
-    res.status(200).json({ 'raw':coins,'summary':countFlips(coins)})
-})
+    var headsCount = 0, tailsCount = 0
+    for (var i = 0; i < coins.length; i++){
+        if (coins[i].valueOf() == "heads") headsCount++
+        else tailsCount++
+    }
 
-app.get('/app/flip/call/:call',(req,res)=>{
-const calling =(req.params.call)
-const coin = coinFlip()
-var result ="win"
-if(coin!=calling){
-    result="lose"
-}
-res.status(200).json({"call":calling,"flip":coin,"result":result})
-
-
-})
-app.use(function(req,res){
-    res.status(404).send("Endpoint does not exist")
-    res.type("text/plain")
+    res.json({"raw" : coins, "summary" : {"heads" : headsCount, "tails" : tailsCount}})
 })
 
+app.get('/app/flip/call/:guess', (req, res) => {
+    
+    var actual = coinFlip()
+    let guess = req.params.guess
+    let result
+    if (actual.valueOf() == guess) result = "win"
+    else result = "lose"
+
+    res.json({"call" : guess, "flip" : actual, "result" : result})
+})
+
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+})
